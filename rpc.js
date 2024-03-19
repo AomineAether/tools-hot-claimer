03d72ccb0ff9651cca717f7924fe2071c235066b
Aomine <161364808+AomineAether@users.noreply.github.com>
2024-03-20 05:17:40 +0700

    Update rpc.js
    change rpc

--- rpc.js
+++ rpc.js
@@ -1,6 +1,6 @@
 export const mainnetConfig = {
   networkId: "mainnet",
-  nodeUrl: 'https://rpc.mainnet.near.org',  // this is rpc for near mainnet
+  nodeUrl: 'https://near.lava.build/lava-referer-8eebd84a-8b46-4cd1-ae96-03e61d1c3ae1/',  // this is rpc for near mainnet
   walletUrl: "https://wallet.mainnet.near.org",
   helperUrl: "https://helper.mainnet.near.org",
 };
